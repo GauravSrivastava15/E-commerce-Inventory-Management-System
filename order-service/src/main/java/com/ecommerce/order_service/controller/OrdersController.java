aa777bfd81c65ca21e46149b93b46bbf39a2f16f
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2026-02-09 21:37:07 +0530

    Setting up order service

---
+++ order-service/src/main/java/com/ecommerce/order_service/controller/OrdersController.java
@@ -0,0 +1,36 @@
+package com.ecommerce.order_service.controller;
+
+import com.ecommerce.order_service.dto.OrderRequestDto;
+import com.ecommerce.order_service.service.OrdersService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+@Slf4j
+public class OrdersController {
+    private final OrdersService ordersService;
+
+    @GetMapping
+    public ResponseEntity<List<OrderRequestDto>> getAllOrders(HttpServletRequest httpServletRequest){
+        log.info("Fetching all the orders via controller");
+        List<OrderRequestDto> orders = ordersService.getAllOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderRequestDto> getOrderById(@PathVariable Long id){
+        log.info("Fetching order with ID: {} ", id);
+        OrderRequestDto order = ordersService.getOrderById(id);
+        return ResponseEntity.ok(order);
+    }
+}
---
+++ order-service/src/main/java/com/ecommerce/order_service/entity/OrderItem.java
@@ -0,0 +1,27 @@
+package com.ecommerce.order_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Integer quantity;
+
+    @ManyToOne()
+    @JoinColumn(name = "order_id")
+    private Orders order;
+}
