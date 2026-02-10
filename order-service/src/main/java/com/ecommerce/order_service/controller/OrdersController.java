95f90813a8aad564009ea292b75b057501bb16b3
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2026-02-10 22:59:09 +0530

    Calling microservice using RestClient

--- order-service/src/main/java/com/ecommerce/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/ecommerce/order_service/controller/OrdersController.java
@@ -20,6 +20,11 @@ import java.util.List;
 public class OrdersController {
     private final OrdersService ordersService;
 
+    @GetMapping("/helloOrders")
+    public String helloOrders(){
+        return "Hello from Order Service";
+    }
+
     @GetMapping
     public ResponseEntity<List<OrderRequestDto>> getAllOrders(HttpServletRequest httpServletRequest){
         log.info("Fetching all the orders via controller");
